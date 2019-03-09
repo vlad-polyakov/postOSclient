6dd1308e0681489bde38a81a7082e441223b7a72
vlad-polyakov <vladislavv.polyakov@mail.ru>
2019-03-09 10:03:53 +0300

    configure headers

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -13,22 +13,26 @@ public class Controller {
     private Connector connector;
     private Response response;
     private HttpStatus httpStatus;
-    public Controller(UI ui){
+
+    public Controller(UI ui) {
         this.ui = ui;
         transformURL = new TransformURL();
     }
 
-    public void sendRequest(String url, String method){
+    public void sendRequest(String url, String method) {
         connector = new Connector();
         httpStatus = new HttpStatus();
-        if(transformURL.checkHost(url)) {
-            String responseStr = connector.sendRequest(transformURL.editHost(url), method);
+        if (transformURL.checkHost(url)) {
+            addHost(url);
+            String responseStr = connector.sendRequest(transformURL.editHost(url), method, this.ui.getHeaderr().fillingHeaders());
             ui.setResponseInfo(responseStr);
             int code = transformURL.getStatusCodeFromResponse(responseStr);
-            System.out.println(httpStatus.getHttpStatusCodes().get(code));
-        }
-        else ui.showAlert();
+            //System.out.println(httpStatus.getHttpStatusCodes().get(code));
+        } else ui.showAlert();
     }
 
-
+    public boolean addHost(String url) {
+        ui.getHeaderr().getAllHeaders().put("Host",transformURL.editHost(url));
+        return true;
+    }
 }
