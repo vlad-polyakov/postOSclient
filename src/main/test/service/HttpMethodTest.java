0c8ae8d8e733bff0cb2b2a8485c3869b0c848054
NarikAoki <hanaaoki16@gmail.com>
2019-03-16 13:58:39 +0300

    add new tests/2

---
+++ src/main/test/service/HttpMethodTest.java
@@ -0,0 +1,31 @@
+package service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpMethodTest {
+    private HttpMethod httpMethod;
+
+    @Before
+    public void init(){
+        httpMethod = new HttpMethod();
+    }
+
+    @Test
+    public void testSuccessGET() {
+        Assert.assertEquals("GET / HTTP/1.1", httpMethod.GET());
+    }
+
+    @Test
+    public void testSuccessPOST() {
+        Assert.assertEquals("POST / HTTP/1.1", httpMethod.POST());
+    }
+
+    @Test
+    public void testSuccessHEAD() {
+        Assert.assertEquals("HEAD / HTTP/1.1", httpMethod.HEAD());
+    }
+}
